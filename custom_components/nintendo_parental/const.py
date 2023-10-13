e33903a2b1375261e33e759cdfb0249d047ff028
Jordan H <jordan@hrvy.uk>
2023-10-13 20:30:58 +0000

    (auth) host middleware via a hass view

--- custom_components/nintendo_parental/const.py
+++ custom_components/nintendo_parental/const.py
@@ -8,9 +8,14 @@ NAME = "Nintendo Switch Parental Controls"
 DOMAIN = "nintendo_parental"
 VERSION = "0.0.4"
 
-MIDDLEWARE_URL = "https://static.system32.uk/hass_middleware.html?return_url={RETURN}&nav_url={NAV}&title={TITLE}&info={INFO}"
+MIDDLEWARE_URL = (
+    "{HASS}/auth/nintendo?return_url={RETURN}&nav_url={NAV}&title={TITLE}&info={INFO}"
+)
 AUTH_CALLBACK_PATH = "/auth/nintendo/callback"
 AUTH_CALLBACK_NAME = "auth:nintendo:callback"
+AUTH_MIDDLEWARE_PATH = "/auth/nintendo"
+AUTH_MIDDLEWARE_NAME = "auth:nintendo"
+AUTH_MIDDLEWARE_CONTENT = "{CWD}/custom_components/nintendo_parental/middleware.html"
 
 SW_OVERRIDE_LIMIT_INVALID = "Attempted to disable device block when screentime limit was already 0, defaulting to 180 minutes."
 
@@ -18,6 +23,6 @@ SW_CONFIGURATION_ENTITIES = {
     "restriction_mode": {
         "icon": "mdi:block-helper",
         "name": "Suspend Software Limit",
-        "value": "parental_control_settings"
+        "value": "parental_control_settings",
     }
 }
