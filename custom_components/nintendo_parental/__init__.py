dd759ca02f0fe8521df571d193adfb7edc85f592
Jordan <jordan@hrvy.uk>
2025-07-29 20:05:49 +0100

    Add bonus time service and update integration setup

--- custom_components/nintendo_parental/__init__.py
+++ custom_components/nintendo_parental/__init__.py
@@ -11,13 +11,19 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.typing import ConfigType
 
 from .const import CONF_SESSION_TOKEN, DOMAIN
 from .coordinator import NintendoParentalConfigEntry, NintendoUpdateCoordinator
 from .repairs import raise_invalid_auth
+from .services import async_setup_services
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.SWITCH, Platform.TIME, Platform.NUMBER]
 
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Nintendo Switch Parental Controls integration."""
+    async_setup_services(hass)
+    return True
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: NintendoParentalConfigEntry
