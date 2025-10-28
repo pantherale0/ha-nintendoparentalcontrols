3f2f631a727a4ed917f44a4f0787c1505bb78364
Jordan Harvey <jordan@hrvy.uk>
2025-10-28 18:59:07 +0000

    handle migration to core

--- custom_components/nintendo_parental/__init__.py
+++ custom_components/nintendo_parental/__init__.py
@@ -1,57 +1,17 @@
 """Custom integration to integrate nintendo_parental with Home Assistant."""
 from __future__ import annotations
 
-from pynintendoparental.authenticator import Authenticator
-from pynintendoparental.exceptions import (
-    InvalidOAuthConfigurationException,
-    InvalidSessionTokenException,
-)
-
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryError
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.typing import ConfigType
 
-from .const import CONF_SESSION_TOKEN, DOMAIN
-from .coordinator import NintendoParentalConfigEntry, NintendoUpdateCoordinator
-from .repairs import raise_invalid_auth
-from .services import async_setup_services
-
-PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.SWITCH, Platform.TIME, Platform.NUMBER]
-
-PLATFORM_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Nintendo Switch Parental Controls integration."""
-    async_setup_services(hass)
-    return True
+from .coordinator import NintendoParentalConfigEntry
+from .repairs import raise_integration_deprecated
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: NintendoParentalConfigEntry
 ) -> bool:
     """Set up Nintendo Switch Parental Controls from a config entry."""
-    try:
-        nintendo_auth = await Authenticator.complete_login(
-            auth=None,
-            response_token=entry.data[CONF_SESSION_TOKEN],
-            is_session_token=True,
-            client_session=async_get_clientsession(hass),
-        )
-    except (InvalidSessionTokenException, InvalidOAuthConfigurationException) as err:
-        raise_invalid_auth(hass, entry)
-        raise ConfigEntryError(
-            translation_domain=DOMAIN,
-            translation_key="auth_expired",
-        ) from err
-    entry.runtime_data = coordinator = NintendoUpdateCoordinator(
-        hass, nintendo_auth, entry
-    )
-    await coordinator.async_config_entry_first_refresh()
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
+    raise_integration_deprecated(hass, entry)
     return True
 
 
@@ -59,7 +19,7 @@ async def async_unload_entry(
     hass: HomeAssistant, entry: NintendoParentalConfigEntry
 ) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return True
 
 async def async_migrate_entry(
     hass: HomeAssistant, entry: NintendoParentalConfigEntry
@@ -82,4 +42,5 @@ async def async_migrate_entry(
                     subentries_data=entry.subentries,
                 )
             )
-        return hass.config_entries.async_remove(entry.entry_id)
+        hass.config_entries.async_remove(entry.entry_id)
+        return True
--- custom_components/nintendo_parental/coordinator.py
+++ custom_components/nintendo_parental/coordinator.py
@@ -15,7 +15,6 @@ from homeassistant.helpers import issue_registry as ir
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import DOMAIN
-from .repairs import raise_no_devices_found, raise_invalid_auth
 
 _LOGGER = logging.getLogger(__name__)
 UPDATE_INTERVAL = timedelta(seconds=60)
@@ -38,23 +37,9 @@ class NintendoUpdateCoordinator(DataUpdateCoordinator[None]):
             update_interval=UPDATE_INTERVAL,
             config_entry=config_entry,
         )
-        self.api = NintendoParental(
-            authenticator, hass.config.time_zone, hass.config.language
-        )
 
     async def _async_update_data(self) -> None:
         """Update data from Nintendo's API."""
-        try:
-            issue_registry = ir.async_get(self.hass)
-            issue_ids_to_track = ["no_devices_found", "invalid_auth"]
-            await self.api.update()
-            for issue_id in issue_ids_to_track:
-                issue = issue_registry.async_get_issue(domain=DOMAIN, issue_id=issue_id)
-                if issue:
-                    ir.async_delete_issue(self.hass, DOMAIN, issue.issue_id)
-        except NoDevicesFoundException:
-            raise_no_devices_found(self.hass, self.config_entry)
-        except InvalidOAuthConfigurationException:
-            raise_invalid_auth(self.hass, self.config_entry)
+        return
 
 type NintendoParentalConfigEntry = ConfigEntry[NintendoUpdateCoordinator]
